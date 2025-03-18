9fd33b869964ca15e5478b1744488b79dcc4ac6e
MHR877 <mehammd57@gmail.com>
2025-03-18 15:51:20 +0100

    Fix the api with http calls

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Redirect all API calls
+        destination: "http://api.cvgenius.stepdevs.click:8000/api/:path*", // Your actual backend
+      },
+    ];
+  },
   ignoreBuildErrors: true,
 };
 
